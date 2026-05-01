e0228ccb25b2cb6a4befde3b6d2cb495517bc877
Mukund <mukundraaz02@gmail.com>
2026-05-01 10:18:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,45 @@
-let parts = [process.platform, process.arch];
-if (process.platform === 'linux') {
-  const { MUSL, familySync } = require('detect-libc');
-  const family = familySync();
-  if (family === MUSL) {
-    parts.push('musl');
-  } else if (process.arch === 'arm') {
-    parts.push('gnueabihf');
-  } else {
-    parts.push('gnu');
+import crypto from 'crypto'
+import { urlAlphabet } from './url-alphabet/index.js'
+const POOL_SIZE_MULTIPLIER = 128
+let pool, poolOffset
+let fillPool = bytes => {
+  if (!pool || pool.length < bytes) {
+    pool = Buffer.allocUnsafe(bytes * POOL_SIZE_MULTIPLIER)
+    crypto.randomFillSync(pool)
+    poolOffset = 0
+  } else if (poolOffset + bytes > pool.length) {
+    crypto.randomFillSync(pool)
+    poolOffset = 0
   }
-} else if (process.platform === 'win32') {
-  parts.push('msvc');
+  poolOffset += bytes
 }
-
-let native;
-try {
-  native = require(`lightningcss-${parts.join('-')}`);
-} catch (err) {
-  native = require(`../lightningcss.${parts.join('-')}.node`);
+let random = bytes => {
+  fillPool((bytes |= 0))
+  return pool.subarray(poolOffset - bytes, poolOffset)
 }
-
-module.exports.transform = wrap(native.transform);
-module.exports.transformStyleAttribute = wrap(native.transformStyleAttribute);
-module.exports.bundle = wrap(native.bundle);
-module.exports.bundleAsync = wrap(native.bundleAsync);
-module.exports.browserslistToTargets = require('./browserslistToTargets');
-module.exports.composeVisitors = require('./composeVisitors');
-module.exports.Features = require('./flags').Features;
-
-function wrap(call) {
-  return (options) => {
-    if (typeof options.visitor === 'function') {
-      let deps = [];
-      options.visitor = options.visitor({
-        addDependency(dep) {
-          deps.push(dep);
-        }
-      });
-
-      let result = call(options);
-      if (result instanceof Promise) {
-        result = result.then(res => {
-          if (deps.length) {
-            res.dependencies ??= [];
-            res.dependencies.push(...deps);
-          }
-          return res;
-        });
-      } else if (deps.length) {
-        result.dependencies ??= [];
-        result.dependencies.push(...deps);
+let customRandom = (alphabet, defaultSize, getRandom) => {
+  let mask = (2 << (31 - Math.clz32((alphabet.length - 1) | 1))) - 1
+  let step = Math.ceil((1.6 * mask * defaultSize) / alphabet.length)
+  return (size = defaultSize) => {
+    let id = ''
+    while (true) {
+      let bytes = getRandom(step)
+      let i = step
+      while (i--) {
+        id += alphabet[bytes[i] & mask] || ''
+        if (id.length === size) return id
       }
-      return result;
-    } else {
-      return call(options);
     }
-  };
+  }
 }
+let customAlphabet = (alphabet, size = 21) =>
+  customRandom(alphabet, size, random)
+let nanoid = (size = 21) => {
+  fillPool((size |= 0))
+  let id = ''
+  for (let i = poolOffset - size; i < poolOffset; i++) {
+    id += urlAlphabet[pool[i] & 63]
+  }
+  return id
+}
+export { nanoid, customAlphabet, customRandom, urlAlphabet, random }
